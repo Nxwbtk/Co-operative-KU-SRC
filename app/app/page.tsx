829e7e2c59aa15833efb5d5bf176866bab7fe793
NewDev <65420147@kmitl.ac.th>
2024-07-19 18:34:33 +0000

    Add tab

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,11 +1,21 @@
 import Image from "next/image";
+"use client"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center min-h-screen justify-center flex-grow bg-gray-100">
-      <h1 className="text-4xl font-bold mb-4">Not Open Yet</h1>
-      <p className="text-lg">
-        This page is not available at the moment. Please check back later.
-      </p>
+      <Tabs defaultValue="account" className="w-full">
+        <TabsList className="w-[750px] border border-black ">
+          <TabsTrigger value="alumni" className="w-full" disabled>สมาคมศิษย์เก่า</TabsTrigger>
+          <TabsTrigger value="club-chart" className="w-full">ทำเนียบนายกสโม</TabsTrigger>
+          <TabsTrigger value="outstanding-alumni" className="w-full">ทำเนียบศิษย์เก่าดีเด่น</TabsTrigger>
+        </TabsList>
+        <TabsContent value="club-chart">
+          Make changes to your account here.
+        </TabsContent>
+        <TabsContent value="outstanding-alumni">Change your password here.</TabsContent>
+      </Tabs>
     </main>
   );
 }
