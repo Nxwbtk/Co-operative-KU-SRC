21122beccb8d180fdde14c097b9ae270918bc412
NewDev <65420147@kmitl.ac.th>
2024-08-04 03:50:55 +0000

    Fix diffence layout

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Topbar } from "./_components/top-bar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +18,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className="flex flex-col min-h-screen">
-          <div className="sticky top-0 w-full z-10">
-            <Topbar />
-          </div>
           <main className="flex-grow overflow-auto">{children}</main>
         </div>
       </body>
