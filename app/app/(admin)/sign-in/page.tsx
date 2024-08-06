54d259e9da95872003540b49c0c80f4f651a1a5b
NewDev <65420147@kmitl.ac.th>
2024-08-06 17:35:13 +0000

    Add package

--- app/app/(admin)/sign-in/page.tsx
+++ app/app/(admin)/sign-in/page.tsx
@@ -8,9 +8,22 @@ import { AuthForm } from "./_components/auth-form";
 // import { buttonVariants } from "@/registry/new-york/ui/button"
 // import { UserAuthForm } from "@/app/(app)/examples/authentication/components/user-auth-form"
 export default async function SignInPage() {
+  const backgroundImageStyle = {
+    backgroundImage:
+      "linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url(/sign-in/src-1.jpg)",
+    backgroundSize: "contain",
+    backgroundPosition: "right",
+    backgroundAttachment: "fixed",
+    width: "100%",
+    height: "100%",
+    position: undefined, // Change the type of position to 'Position | undefined'
+    top: 0,
+    left: 0,
+    zIndex: -1,
+  };
   return (
     <>
-      <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="container relative min-h-screen flex-col items-center justify-center grid md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
@@ -24,51 +37,15 @@ export default async function SignInPage() {
             <AuthForm />
           </div>
         </div>
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-          <div className="absolute inset-0 bg-zinc-900" />
-          {/* <div className="relative z-20 flex items-center text-lg font-medium">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-6 w-6"
-            >
-              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-            </svg>
-            Acme Inc
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex items-center justify-center" style={backgroundImageStyle}>
+          {/* <div className="absolute inset-0 bg-zinc-900" /> */}
+          {/* <div className="relative z-20">
           </div> */}
-          <div className="relative z-20 mt-auto">
-            <blockquote className="space-y-2">
-              <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before.&rdquo;
-              </p>
-              <footer className="text-sm">Sofia Davis</footer>
-            </blockquote>
+          <div className="flex justify-center items-center">
+          <Image src={"/sign-in/KUSRC.png"} alt={""} width={400} height={400} />
           </div>
         </div>
       </div>
-      {/* <div className="md:hidden">
-        <Image
-          src="/examples/authentication-light.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/authentication-dark.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="hidden dark:block"
-        />
-      </div> */}
     </>
   );
 }
