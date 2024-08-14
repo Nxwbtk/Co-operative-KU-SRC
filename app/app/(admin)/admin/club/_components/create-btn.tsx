11a9294aa076b4a428bbe4d9324b33311d43b829
nxwbtk <buntakan0703@gmail.com>
2024-08-14 11:20:37 +0000

    feat: Update file paths for ManageClubScreen and PortalScreen components
    This commit updates the file paths for the `ManageClubScreen` and `PortalScreen` components in the admin club module. The file paths are modified to match the new folder structure and naming conventions. These changes ensure that the components are correctly imported and used in the admin club module.

--- app/app/(admin)/admin/club/_components/create-btn.tsx
+++ app/app/(admin)/admin/club/_components/create-btn.tsx
@@ -23,7 +23,6 @@ export const CreateBtn = () => {
       <DialogHeader>
         <DialogTitle>Edit profile</DialogTitle>
         <DialogDescription>
-          Make changes to your profile here. Click save when you're done.
         </DialogDescription>
       </DialogHeader>
       <div className="grid gap-4 py-4">
