7cd2d3c077aea48487dec7ff9d45e31a24b4fabf
NewDev <65420147@kmitl.ac.th>
2024-09-25 15:45:23 +0000

    Fix build failed

--- app/app/(admin)/sign-in/forgot-password/page.tsx
+++ app/app/(admin)/sign-in/forgot-password/page.tsx
@@ -1,8 +1,12 @@
 "use client";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import * as z from "zod";
 import { useForm, UseFormReturn } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
@@ -12,19 +16,14 @@ import { InputFormField } from "@/components/input-form-field/input-field";
 import { postRequestOTP } from "./_actions/post-request-otp";
 import { useState } from "react";
 import { Loader2Icon } from "lucide-react";
-
-export const forgotPasswordSchema = z.object({
-  email: z.string().email(),
-});
-
-export type TForgotPasswordSchema = z.infer<typeof forgotPasswordSchema>;
+import { forgotPasswordSchema, TForgotPasswordSchema } from "./types";
 
 export default function ForgotPasswordPage() {
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
   const handleBack = () => {
     router.push("/sign-in");
-  }
+  };
   const form: UseFormReturn<TForgotPasswordSchema> =
     useForm<TForgotPasswordSchema>({
       resolver: zodResolver(forgotPasswordSchema),
@@ -38,15 +37,15 @@ export default function ForgotPasswordPage() {
     const payload = {
       email: data.email,
       otp: otp.toString(),
-    }
+    };
     const res = await postRequestOTP(payload);
     if (res.error) {
       toast.error("เกิดข้อผิดพลาด");
       setIsLoading(false);
-      return ;
+      return;
     }
     toast.success("สำเร็จ", {
-      description: "กรุณาตรวจสอบอีเมลของคุณ"
+      description: "กรุณาตรวจสอบอีเมลของคุณ",
     });
     router.push("/sign-in/forgot-password/success");
   };
@@ -64,38 +63,47 @@ export default function ForgotPasswordPage() {
           </CardHeader>
           <CardContent>
             <Form {...form}>
-            <form
-              onSubmit={form.handleSubmit(handleSubmit)}
-              className="space-y-4"
-            >
-              <div className="space-y-2">
-                {/* <Input
+              <form
+                onSubmit={form.handleSubmit(handleSubmit)}
+                className="space-y-4"
+              >
+                <div className="space-y-2">
+                  {/* <Input
                   type="email"
                   placeholder="อีเมล"
                   required
                   className="border-[#F5B21F] focus:ring-[#302782]"
                 /> */}
-                <InputFormField
-                  type="email"
-                  placeholder="อีเมล"
-                  form={form}
-                  name="email"
-                  label=""
-                />
-              </div>
-              <div className="flex flex-row">
-              <Button
-                type="submit"
-                className="w-full rounded-none"
-                disabled={isLoading}
-              >
-                {isLoading ? <Loader2Icon className="animate-spin" size={16} /> : "ส่งลิงก์เพื่อรีเซ็ตรหัสผ่าน"}
-              </Button>
-              <Button variant="secondary" type="button" className="rounded-none" onClick={handleBack}>
-                กลับไปหน้าเข้าสู่ระบบ
-              </Button>
-              </div>
-            </form>
+                  <InputFormField
+                    type="email"
+                    placeholder="อีเมล"
+                    form={form}
+                    name="email"
+                    label=""
+                  />
+                </div>
+                <div className="flex flex-row">
+                  <Button
+                    type="submit"
+                    className="w-full rounded-none"
+                    disabled={isLoading}
+                  >
+                    {isLoading ? (
+                      <Loader2Icon className="animate-spin" size={16} />
+                    ) : (
+                      "ส่งลิงก์เพื่อรีเซ็ตรหัสผ่าน"
+                    )}
+                  </Button>
+                  <Button
+                    variant="secondary"
+                    type="button"
+                    className="rounded-none"
+                    onClick={handleBack}
+                  >
+                    กลับไปหน้าเข้าสู่ระบบ
+                  </Button>
+                </div>
+              </form>
             </Form>
           </CardContent>
         </Card>
